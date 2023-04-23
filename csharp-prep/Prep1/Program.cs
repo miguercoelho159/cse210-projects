73288d75f943bbe67cf36fe059627700eaf8042a
Miguel <miguel.coelho1599@gmail.com>
2023-04-22 23:25:11 -0700

    Completed prep1 assignment.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
+        //Could also do as below:
+        //string full_bond = ($"Your name is {last_name}, {first_name} {last_name}.");
+        //Console.Write(full_bond);
     }
 }
