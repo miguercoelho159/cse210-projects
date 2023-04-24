9bb7cc36426966252ed205046622d2e9d0e60c9c
Miguel <miguel.coelho1599@gmail.com>
2023-04-24 00:41:10 -0700

    Completed prep 1 and 2 again. ;)

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello there, what is your grade percentage?");
+        string user_grade = Console.ReadLine();
+        int final_grade = int.Parse(user_grade);
+
+        string letter = "";
+
+        if (final_grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (final_grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (final_grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (final_grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your current grade is: {letter}.");
+
+        if (final_grade >= 70)
+        {
+            Console.WriteLine($"Congrats! You have passed with an {letter}! :)");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you did not pass. :,(");
+        }
     }
 }
