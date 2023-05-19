5990d369ab4aade11e2efa1dc55d34a26115b95c
Miguel <miguel.coelho1599@gmail.com>
2023-05-18 22:40:49 -0700

    Completed encapsulation assingment.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+
+        Fraction firstFraction = new Fraction();
+
+        Console.WriteLine(firstFraction.GetFractionString());
+        Console.Write(firstFraction.GetDecimalValue());
+
+
+        Fraction secondFraction = new Fraction(5);
+
+        Console.WriteLine(secondFraction.GetFractionString());
+        Console.Write(secondFraction.GetDecimalValue());
+
+
+        Fraction thirdFraction = new Fraction(3, 4);
+
+        Console.WriteLine(thirdFraction.GetFractionString());
+        Console.Write(thirdFraction.GetDecimalValue());
+
+
+        Fraction fourthFraction = new Fraction(1, 3);
+
+        Console.WriteLine(fourthFraction.GetFractionString());
+        Console.Write(fourthFraction.GetDecimalValue());
+        
     }
 }
