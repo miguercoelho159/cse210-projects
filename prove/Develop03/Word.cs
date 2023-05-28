09d815405cff623044b83067dbcc21d074b3ec99
Miguel <miguel.coelho1599@gmail.com>
2023-05-27 23:39:27 -0700

    Finished Word.cs file / started Scripture.

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,43 @@
+using System.Text;
+
+public class Word
+{
+
+    private bool _hidden;
+    private string _text;
+    public Word(string text)
+    {
+        _text = text;
+        _hidden = false;
+    }
+
+    public void Hide()
+    {
+        _hidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return _hidden;
+    }
+
+    public string GetWord()
+    {
+
+        if (_hidden == true)
+        {
+            StringBuilder tempString = new StringBuilder("");
+            
+            for (int i = 0; i<_text.Length; i++)
+            {
+            tempString.Append("-");
+            }
+            return tempString.ToString();
+        }
+        else
+        {
+        return _text;
+        }    
+    
+    }
+}
